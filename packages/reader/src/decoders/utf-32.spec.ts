fa11a917bf0ebf01cf052124de49bb4b64d69b96
Tao Cumplido <taocumplido@gmail.com>
2022-02-02 11:37:06 +0100

    update pkg reader to 0.1.6 - fix utf-32 upper codepoint bound check

--- packages/reader/src/decoders/utf-32.spec.ts
+++ packages/reader/src/decoders/utf-32.spec.ts
@@ -9,10 +9,13 @@ import { ReadError } from '../read-error.js';
 const type = DataType.char(Encoding.UTF32, ByteOrder.BigEndian);
 
 test('valid', ({ deepEqual }) => {
-	const reader = new BinaryReader(new Uint8Array([...[0x00, 0x00, 0xd7, 0xff], ...[0x00, 0x00, 0xe0, 0x00]]));
+	const reader = new BinaryReader(
+		new Uint8Array([...[0x00, 0x00, 0xd7, 0xff], ...[0x00, 0x00, 0xe0, 0x00], ...[0x00, 0x10, 0xff, 0xff]]),
+	);
 
 	deepEqual(reader.next(type), { value: String.fromCodePoint(0xd7ff), byteLength: 4 });
 	deepEqual(reader.next(type), { value: String.fromCodePoint(0xe000), byteLength: 4 });
+	deepEqual(reader.next(type), { value: String.fromCodePoint(0x10ffff), byteLength: 4 });
 });
 
 test('invalid', ({ deepEqual, throws }) => {
@@ -26,5 +29,10 @@ test('invalid', ({ deepEqual, throws }) => {
 	deepEqual(
 		throws(() => new BinaryReader(new Uint8Array([0x00, 0x00, 0xdf, 0xff])).next(type)),
 		new ReadError(message, type, new Uint8Array([0x00, 0x00, 0xdf, 0xff])),
+	);
+
+	deepEqual(
+		throws(() => new BinaryReader(new Uint8Array([0x00, 0x11, 0x00, 0x00])).next(type)),
+		new ReadError(message, type, new Uint8Array([0x00, 0x11, 0x00, 0x00])),
 	);
 });
--- packages/reader/src/decoders/utf-32.ts
+++ packages/reader/src/decoders/utf-32.ts
@@ -6,7 +6,7 @@ import { ReadError } from '../read-error.js';
 export default (type: DataChar, reader: BinaryReader): DataValue<string> => {
 	const codePoint = reader.next(DataType.int({ signed: false, byteLength: 4 }, type.byteOrder)).value;
 
-	if (codePoint >= 0xd800 && codePoint < 0xe000) {
+	if ((codePoint >= 0xd800 && codePoint < 0xe000) || codePoint > 0x10ffff) {
 		throw new ReadError(`invalid UTF-32 code point`, type, reader.buffer.slice(reader.offset - 4, reader.offset));
 	}
 
