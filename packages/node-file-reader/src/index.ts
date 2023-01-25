ef46d4e18af7d002cc20d99de939f720efe4a8a4
Tao Cumplido <taocumplido@gmail.com>
2023-01-25 21:02:47 +0100

    add more reexports

--- packages/node-file-reader/src/index.ts
+++ packages/node-file-reader/src/index.ts
@@ -1,2 +1,11 @@
-export { ByteOrder, DataType, Encoding, ReadError } from '@nishin/reader';
+export type {
+	AsyncDataReader,
+	AsyncDataReaderLike,
+	BytesValue,
+	DataReader,
+	DataReaderState,
+	FloatBytes,
+	SafeIntBytes,
+} from '@nishin/reader';
+export { repeat, repeatAsync, ByteOrder, DataType, Encoding, ReadError, ReadMode } from '@nishin/reader';
 export { NodeFileReader } from './node-file-reader.js';
